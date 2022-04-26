e07fda14dfd41286efda5234b0ff99744fd5b17d
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2022-04-26 16:58:35 +0200

    background nav

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -4,24 +4,40 @@ const btn2 = document.querySelector('#info');
 const text2 = document.querySelector('.nav3');
 
 let visible = false;
+let active = false;
 
 btn.addEventListener('click', () => {
    visible = !visible;
-   visible ? text.classList.add('visible') : text.classList.remove('visible'), text2.classList.remove('visible');
+    visible ? text.classList.add('visible') : text.classList.remove('visible'), text2.classList.remove('visible'), btn2.classList.remove('active');
+
+
 });
 
 btn2.addEventListener('click', () => {
+    active = !active;
     text2.classList.toggle('visible');
+    btn2.classList.toggle('active');
 });
 
+
+
+document.querySelector('#dossierImage').addEventListener('hidden.bs.modal', () => {
+    text2.classList.remove('visible');
+    btn2.classList.remove('active');
+});
+
+/*
 // Permet de supprimer toute les fenetre ouverte lors de la fermeture de la modal
 document.querySelector('.ferm').addEventListener('click', () => {
     visible = !visible;
     visible ? text.classList.add('visible') : text.classList.remove('visible'), text2.classList.remove('visible');
+    btn2.classList.remove('active');
 });
 
 // Permet de supprimer toute les fenetre ouverte lors de la fermeture de la modal
 document.querySelector('.btn-secondary').addEventListener('click', () => {
     visible = !visible;
     visible ? text.classList.add('visible') : text.classList.remove('visible'), text2.classList.remove('visible');
-});
+    btn2.classList.remove('active');
+
+});*/
